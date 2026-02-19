0d4719391b68801d52837c3622df885e5425e471
Alexey Chekhirov <alexeychekhirov@Alexeys-iMac.local>
2026-02-20 01:52:15 +0300

    cleanup and refactor

--- blog/forms.py
+++ blog/forms.py
@@ -2,7 +2,15 @@ from django import forms
 
 from .models import Post
 
+
 class PostForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Добавляем классы Bulma к каждому полю
+        for field_name in self.fields:
+            self.fields[field_name].widget.attrs['class'] = 'input is-rounded'
+
+
     class Meta:
         model = Post
-        fields = ('version', 'body')
+        fields = ['version', 'body']  # укажите нужные поля
--- main/views.py
+++ main/views.py
@@ -1,26 +1,37 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from blog.models import Post
-# Create your views here.
-
 from blog.forms import PostForm
-
-def add(request):
-    form = PostForm()
-    posts = Post.objects.all()
-    return render(request, 'main/versions.html', {'posts': posts })
+from django.http import HttpResponse
 
 def index(request):
-    return render(request,"main/index.html")
+    return render(request, "main/index.html")
+
+
+def home(request):
+    return render(request, "main/index.html")
+
 
 def about(request):
-    return render(request,"main/about.html")
+    return render(request, "main/about.html")
+
 
 def versions(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-created_at')  # сортировка по дате
     form = PostForm()
+
     if request.method == "POST":
         form = PostForm(request.POST)
-        form.save()
-        return redirect(versions)
-    return render(request,"main/versions.html",{'posts': posts, 'form': form})
+        if form.is_valid():
+            form.save()
+            return redirect('versions')  # перенаправление по имени URL
+
+
+    return render(
+        request,
+        "main/versions.html",
+        {'posts': posts, 'form': form}
+    )
+
+# Функция add() удалена — логика перенесена в versions()
+def test_view(request):
+    return HttpResponse("Test OK")
