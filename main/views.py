89423ce075218e11bb1cedaee0fc7c67770de5c5
Alexey Chekhirov <fwea2015@yandex.ru>
2026-05-11 13:35:04 +0000

    markup little fix

--- main/views.py
+++ main/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from importlib import import_module
 
-version = import_module('__version__').VERSION
-
 def home(request):
     return render(request, "main/index.html")
 
